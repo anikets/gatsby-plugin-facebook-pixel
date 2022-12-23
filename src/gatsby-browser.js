d5599b3cd5db7f78b527bc6f2bda9142c503b830
Aniket A. Suryavanshi <aniket@onjoyride.com>
2022-12-23 12:31:27 +0530

    param update

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,4 +1,4 @@
-exports.onRouteUpdate = function () {
+exports.onRouteUpdate = function (apiCallbackContext, pluginOptions) {
   // Don't track while developing.
   if (process.env.NODE_ENV === `production` && typeof fbq === `function` && !pluginOptions.disableViewContent) {
     fbq("track", "ViewContent");
